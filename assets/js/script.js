7b9cc206ef1a321cbb3667c61c3ca27b0648bb91
Moore <mooreart95@gmail.com>
2022-07-01 14:22:56 -0600

    5 day headers

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,6 @@ var searchBtn = document.getElementById("search-btn");
 console.log(searchBtn);
 
 var apiKey = "3fe813626b12b8d2b8762d1a88477d61"
-
 //call API to get weather data
 function fetchWeather(lat, lon, city) {
     var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&dt&appid=3fe813626b12b8d2b8762d1a88477d61'
@@ -18,20 +17,19 @@ function fetchWeather(lat, lon, city) {
     })
 }
 
-function citySearch() {
-    var city = searchBar;
-    fetchWeather(city);
+//get todays date
+var getDate = function() {
+    var currentDate = moment().format("dddd, MMMM Do YYYY");
+    return currentDate;
+}
+
+var displayHeaders = function() {
+    document.querySelector("#forecast-1").textContent = getDate().add(1, "d").format("dddd");
+    document.querySelector("#forecast-2").textContent = moment().add(2, "d").format("dddd");
+    document.querySelector("#forecast-3").textContent = moment().add(3, "d").format("dddd");
+    document.querySelector("#forecast-4").textContent = moment().add(4, "d").format("dddd");
+    document.querySelector("#forecast-5").textContent = moment().add(5, "d").format("dddd");
 };
 
-searchBtn.addEventListener('click', citySearch);
-
-//put weather data into current day box
-
-//put 5 day data into individual boxes
-
-//save searched city in local storage
-
-//show recently searched cities under searchbar
-
-//populate data on city submit 
-
+ 
+displayHeaders();
